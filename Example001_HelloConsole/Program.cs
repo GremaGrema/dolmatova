9f92619914edc0eb91ecc4c9c47fc821276d920b
Евгений Долматов <C:\Users\datat\AppData\Roaming\The Bat!>
2022-08-15 16:12:37 +0700

    задачи

--- Example001_HelloConsole/Program.cs
+++ Example001_HelloConsole/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("2 Hello, World!");
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 3);
+        //index = index + 1;
+        index++;
+    }
+}
+
+
+int[] array = new int[3];
+
