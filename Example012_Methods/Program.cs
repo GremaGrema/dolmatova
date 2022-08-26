eaa790d176268f3ef5e2cf5e26c958a4231da02d
Евгений Долматов <C:\Users\datat\AppData\Roaming\The Bat!>
2022-08-27 01:42:37 +0700

    lection4

---
+++ Example012_Methods/Program.cs
@@ -0,0 +1,63 @@
+﻿// Вид 1
+void Method1()
+{
+    Console.WriteLine("Автор ...");
+}
+// Method1();
+
+// Вид 2
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+//Method2(msg: "Текст сообщения");
+// Аргументы именованные:
+
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         Console.WriteLine(msg);
+//         i++;//(энкримент)
+//     }
+// }
+// Method21(msg: "Текст",count: 4);
+
+
+// Вид 3
+
+// int Metod3()//- не принимает никакие аргументы
+// {
+//     return DataTime.Now.Year;// - обязательное использование оператора return,
+// }
+// int year = Metod3();
+// // Console.WriteLine(year);
+
+
+// Вид 4
+
+// string Metod4(int count, string text)
+// {
+//     i = 0;
+//     string result = String.Empty;
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+// string res = Metod4(10, "asdf");
+// Console.WriteLine(res);
+
+// Цикл В Цикле
+
+for ( int i = 2; i < 10; i++)
+{
+    for ( int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} * {j} = {i * j} ");
+    }
+    Console.WriteLine();
+}
---
+++ Example013_Factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+// int Factorial (int n)
+// {
+//     if (n == 1) return 1;
+//     else return n * Factorial (n - 1);
+// }
+// Console.WriteLine(Factorial(5)); // 1*2*3=6
+
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+double Fibonaci (int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonaci(n - 1) + Fibonaci(n - 2);
+}
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine(Fibonaci(i));
+}
